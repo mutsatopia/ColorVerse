d8dc4a1d31cd38737ed0bbe63b016b62024031fc
searchmoon <moonding901212@gmail.com>
2022-01-01 00:29:41 +0900

    💄 메인, 푸터 UI 개선

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,26 @@
+//btn-gen 클릭시 
 const genBtn = document.querySelector(".color-get .btn-gen");
-const bgSect = document.querySelector(".bg-default");
+const bgChange = document.querySelector(".bg-color");
+
+const main = document.querySelector("main");
+const bottomBox = document.querySelector(".bottom-box");
 
 genBtn.addEventListener("click", () => {
-  const colorCode = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  const colorCode2 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  bgSect.style.background = `linear-gradient(45deg, ${colorCode}, ${colorCode2})`;
+  bgChange.classList.add('changed');
+  main.classList.remove("hide");
+  bottomBox.classList.remove("hide");
+
+  const artRec = document.querySelector('.art-rec');
+  artRec.scrollIntoView({ behavior: "smooth", block: "end"});
 });
 
-console.log(colorCode);
+// bottom-box 안에 floating 효과
+function floatingObject(selector) {
+  gsap.to(selector, 1, {
+    y: 15,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut
+  });
+}
+floatingObject('.floating');
