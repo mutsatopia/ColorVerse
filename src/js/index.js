f8a62485813a9d43b74cb40b9ee0ac921a84de23
HojinAn <asdv94@naver.com>
2022-01-01 14:28:33 +0900

    ✨ 스크롤 fadein 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
-//btn-gen 클릭시 
-const genBtn = document.querySelector(".color-get .btn-gen");
+//btn-gen 클릭시
+const genBtn = document.querySelector(".btn-gen");
 const bgChange = document.querySelector(".bg-color");
 
 const main = document.querySelector("main");
-const bottomBox = document.querySelector(".bottom-box");
+const bottomBox = document.querySelector(".bg-bottom");
 
 genBtn.addEventListener("click", () => {
-  bgChange.classList.add('changed');
+  bgChange.classList.add("changed");
   main.classList.remove("hide");
   bottomBox.classList.remove("hide");
 
-  const artRec = document.querySelector('.art-rec');
-  artRec.scrollIntoView({ behavior: "smooth", block: "end"});
+  const artRec = document.querySelector(".art-rec");
+  artRec.scrollIntoView({ behavior: "smooth", block: "end" });
 });
 
 // bottom-box 안에 floating 효과
@@ -20,7 +20,48 @@ function floatingObject(selector) {
     y: 15,
     repeat: -1,
     yoyo: true,
-    ease: Power1.easeInOut
+    ease: Power1.easeInOut,
   });
 }
-floatingObject('.floating');
+floatingObject(".floating");
+
+// paletts 나타내기
+const scrollElements = document.querySelectorAll(".js-scroll");
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
